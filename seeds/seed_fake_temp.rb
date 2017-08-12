842696949337548d1c903e5cdf46490ca0858c3d
Kevin Coleman <kevin.coleman@zugata.com>
2017-08-12 15:03:35 +0800

    update github

--- seeds/seed_fake_temp.rb
+++ seeds/seed_fake_temp.rb
@@ -19,34 +19,58 @@ def fake_temp(last_temp)
   new_temp
 end
 
+def fake_humidity(last_temp)
+  new_temp = last_temp + (Random.rand(4) - 2).to_f / 4 + 0.2.to_f
+  send_humidity(new_temp)
+  sleep(1)
+  puts 'sent ' + new_temp.to_s + '%'
+  new_temp
+end
+
 def send_grade(grade)
-  uri = URI.parse('https://api-m2x.att.com/v2/devices/5da74d20d9ec77aee3cac8c89be58bbc/streams/Grade-Quality/value')
+  uri = URI.parse('https://api-m2x.att.com/v2/devices/9346b77b144e5e00e40cc7f9e6220840/streams/Grade-Quality/value')
   request = Net::HTTP::Put.new(uri)
   request.content_type = 'application/json'
-  request['X-M2x-Key'] = '71575488402d331035ed380ab1e43338'
+  request['X-M2x-Key'] = 'de395ffe039fc422ee72ccd017165105'
   request.body = JSON.dump('value' => grade.to_s)
   req_options = { use_ssl: uri.scheme == 'https' }
   response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
     http.request(request)
   end
-  response.body
+  puts response.body
 end
 
 def send_temp(temp)
-  uri = URI.parse('https://api-m2x.att.com/v2/devices/5da74d20d9ec77aee3cac8c89be58bbc/streams/temperature/value')
+  uri = URI.parse('https://api-m2x.att.com/v2/devices/9346b77b144e5e00e40cc7f9e6220840/streams/temperature/value')
   request = Net::HTTP::Put.new(uri)
   request.content_type = 'application/json'
-  request['X-M2x-Key'] = '71575488402d331035ed380ab1e43338'
+  request['X-M2x-Key'] = 'de395ffe039fc422ee72ccd017165105'
   request.body = JSON.dump('value' => temp.to_s)
   req_options = { use_ssl: uri.scheme == 'https' }
   response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
     http.request(request)
   end
-  response.body
+  puts response.body
 end
 
-send_grade(1)
-last_temp = 13
-100.times do
-  last_temp = fake_temp(last_temp)
+def send_humidity(temp)
+  uri = URI.parse('https://api-m2x.att.com/v2/devices/9346b77b144e5e00e40cc7f9e6220840/streams/Humidity/value')
+  request = Net::HTTP::Put.new(uri)
+  request.content_type = 'application/json'
+  request['X-M2x-Key'] = 'de395ffe039fc422ee72ccd017165105'
+  request.body = JSON.dump('value' => temp.to_s)
+  req_options = { use_ssl: uri.scheme == 'https' }
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
+  puts response.body
+end
+
+# send_grade(1)
+last_temp = 74
+last_humidity = 20
+
+4.times do
+  # last_temp = fake_temp(last_temp)
+  last_humidity = fake_humidity(last_humidity)
 end
